e3ff58ae2b03c6ec0113f9917503ada5a6cf3e24
westbli <l_westberg@msn.com>
2016-02-19 12:11:58 +0100

    Time task körs nu i 12 minuter bara

--- GruppArbete/src/application/ProcessaEpost.java
+++ GruppArbete/src/application/ProcessaEpost.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,24 +13,33 @@ import javax.mail.*;
 import javax.mail.internet.*;
 
 public class ProcessaEpost extends TimerTask {
-	private static String USER_NAME = "nackademiskaauktion"; 
-    private static String PASSWORD = "Gunnar4president";
+	private static String USER_NAME = "nackademiskaauktion";
+	private static String PASSWORD = "Gunnar4president";
 	private Connection connection;
+	private CallableStatement cStmt;
 	private Statement stmt;
 	private ResultSet rs;
+	private Timer timer;
+	private int count;
 
 	public ProcessaEpost() {
 		connection = null;
 		stmt = null;
 		rs = null;
+		cStmt = null;
+		count = 0;
 	}
-	
+
 	private void skickaEpost() throws SQLException {
 		try {
 			if (this.connection == null) {
 				connection = Model.MODEL.getConnection();
 			}
 			if (connection != null) {
+				cStmt = connection.prepareCall("{call arkiveraAuktion}");
+				if (cStmt.execute()) {
+					System.out.println("arkiveraAuktion anropat");
+				}
 				stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 				rs = stmt.executeQuery("select * from epost where `status` = 'köat'");
 				rs.beforeFirst();
@@ -37,7 +47,7 @@ public class ProcessaEpost extends TimerTask {
 					String mottagare = rs.getString("mottagare");
 					String innehåll = rs.getString("innehåll");
 					String ämne = rs.getString("ämne");
-					this.composeEmail(mottagare,innehåll,ämne);
+					this.composeEmail(mottagare, innehåll, ämne);
 				}
 				String sql = "update epost set `status` = 'skickat' where `status` = 'köat' ";
 				stmt.executeUpdate(sql);
@@ -45,72 +55,71 @@ public class ProcessaEpost extends TimerTask {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			stmt.close();
-			rs.close();
+			if (cStmt != null)
+				cStmt.close();
+			if (stmt != null)
+				stmt.close();
+			if (rs != null)
+				rs.close();
 		}
 	}
-	
-	private void composeEmail(String mottagare, String innehåll, String ämne){
+
+	private void composeEmail(String mottagare, String innehåll, String ämne) {
 		String from = USER_NAME;
-        String pass = PASSWORD;
-        String[] to = { mottagare };
-        String subject = ämne;
-        String body = innehåll;
-        Properties props = System.getProperties();
-        String host = "smtp.gmail.com";
-        
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", from);
-        props.put("mail.smtp.password", pass);
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
+		String pass = PASSWORD;
+		String to = mottagare;
+		String subject = ämne;
+		String body = innehåll;
+		Properties props = System.getProperties();
+		String host = "smtp.gmail.com";
 
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.user", from);
+		props.put("mail.smtp.password", pass);
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
 
-        try {
-            message.setFrom(new InternetAddress(from));
-            InternetAddress[] toAddress = new InternetAddress[to.length];
+		Session session = Session.getDefaultInstance(props);
+		MimeMessage message = new MimeMessage(session);
 
-            for( int i = 0; i < to.length; i++ ) {
-                toAddress[i] = new InternetAddress(to[i]);
-            }
-
-            for( int i = 0; i < toAddress.length; i++) {
-                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-            }
-
-            message.setSubject(subject);
-            message.setText(body);
-            Transport transport = session.getTransport("smtp");
-            transport.connect(host, from, pass);
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-        }
-        catch (AddressException ae) {
-            ae.printStackTrace();
-        }
-        catch (MessagingException me) {
-            me.printStackTrace();
-        }
+		try {
+			message.setFrom(new InternetAddress(from));
+			InternetAddress toAddress = new InternetAddress(to);
+			message.addRecipient(Message.RecipientType.TO, toAddress);
+			message.setSubject(subject);
+			message.setText(body);
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, from, pass);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		} catch (AddressException ae) {
+			ae.printStackTrace();
+		} catch (MessagingException me) {
+			me.printStackTrace();
+		}
 	}
-	
-	public void startTask(){
+
+	public void startTask() {
 		System.out.println("Timer task started at:" + new Date());
 		this.connection = Model.MODEL.getConnection();
-		Timer timer = new Timer(false);
-		timer.scheduleAtFixedRate(this, 0, 3 * 60000);// run every 3 minutes
+		timer = new Timer(false);
+		timer.scheduleAtFixedRate(this, 0, 2 * 60000);// run every 2 minute
 	}
 
 	@Override
 	public void run() {
-		try {
-			skickaEpost();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		count++;
+		if (count >= 6) { //Just for demonstration purposes - task only runs for 12 minutes.
+			timer.cancel();
+			timer.purge();
+			return;
+		} else {
+			try {
+				skickaEpost();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-
 	}
-
 }
